76888ca9a21df681ebe2a9b2ef115e7e8d314c4c
tnkmr08 <88134070+tnkmr08@users.noreply.github.com>
2021-08-25 21:05:18 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,5 +7,14 @@ class Item < ApplicationRecord
   belongs_to :day
   belongs_to :delively_fee
   belongs_to :prefecture
+
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, price: true
   
+
 end
