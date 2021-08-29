6adeed5bfd9a2892b537f7e4eb4e00b008a70eb4
tnkmr08 <88134070+tnkmr08@users.noreply.github.com>
2021-08-29 20:42:31 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,8 @@ class ItemsController < ApplicationController
   end  
 
   def destroy
-    if @item.destroy
+    if current_user.id == @item.user_id 
+      @item.destroy
       redirect_to root_path
     end  
   end  
